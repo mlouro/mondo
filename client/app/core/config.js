06801e35d5d1f5a6f6599cb9c4dbaedff33b9233
Marco Louro <mlouro@gmail.com>
2014-04-01 15:40:02 +0200

    added missing files

---
+++ client/app/core/config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+var config = {
+  // Recurring options.
+  'recurring': {
+    'once': 'Just once',
+    '1week': 'Every week',
+    '2weeks': 'Every two weeks',
+    '1month': 'Every month'
+  }
+}
+
+module.exports = config;
